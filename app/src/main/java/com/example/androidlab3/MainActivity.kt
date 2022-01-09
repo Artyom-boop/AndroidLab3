e0d257cbbcb72b4ae0c3a93fb22633c52aec7d5e
Artyom_Sidorov_3530901/90004 <artyom.sidoroff2014@yandex.ru>
2022-01-09 20:53:52 +0300

    add Home button

--- app/src/main/java/com/example/androidlab3/MainActivity.kt
+++ app/src/main/java/com/example/androidlab3/MainActivity.kt
@@ -2,11 +2,36 @@ package com.example.androidlab3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.navigation.findNavController
 import com.example.androidlab3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
         setContentView(ActivityMainBinding.inflate(layoutInflater).root)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.activity_about -> {
+                findNavController(R.id.host_fragment).navigate(R.id.menu_about)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
